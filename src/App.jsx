382c39545a6d7f6f4780f415c77e25a06ba26102
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-04 18:43:17 -0300

    create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import Post from "./components/Post";
+
+import styles from "./App.module.css";
+
 import "./global.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Post author="Lucas" content="Oi" />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorem sit tempore beatae accusamus officiis deserunt repudiandae, assumenda totam laudantium omnis modi quia ea quidem quo? Obcaecati ratione culpa ab?"
-      />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Lucas" content="Oi" />
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorem sit tempore beatae accusamus officiis deserunt repudiandae, assumenda totam laudantium omnis modi quia ea quidem quo? Obcaecati ratione culpa ab?"
+          />
+          <Post />
+        </main>
+      </div>
     </>
   );
 }
